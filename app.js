3d8d1ab89ab7df57cea427917d1b19ebc1ef45d4
yongrenwang <czie.alexmak@gmail.com>
2017-02-18 20:07:47 +0800

    Developed plan for web application

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-//Problem:
+//Problem: We need a simple way to look at a user's badge count and Javascript points from a web browser.
 
-//Solution::
+//Solution::  Use Node.js to perform the profile look ups and server our template via HTTP.
+
+//1. Create a web server
+
+//2. Handle HTTP route GET / and POST / i.e. Home
+	//if url == "/" && GET
+		//show search
+	//if url == "/" && POST
+		//redirect to /:username
+
+//3. Handle HTTP route GET /:username i.e. /yongrenwang
+	//if url == '/....'
+		//get json from Treehouse
+			//on "end"
+				//show profile
+			//on "error"
+				//show error
+
+//4. Function that handles the reading of files and merge in values
+	//read from file and get a string
+		//merge values in to string
